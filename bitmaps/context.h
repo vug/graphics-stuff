b7f606eb124688d3b1ae959e3ca29dbc090c303b
Uğur Güney <ugurguney@gmail.com>
2022-05-11 21:11:51 -0400

    format

--- bitmaps/context.h
+++ bitmaps/context.h
@@ -116,13 +116,14 @@ public:
       int y = y0;
       for (int x = x0; x != x1; x += dx)
       {
-        if (accumulate > threshold) {
+        if (accumulate > threshold)
+        {
           y += dy;
           threshold += 1;
         }
         pixels[y][x] = c;
         accumulate += mAbs;
-      }      
+      }
     }
     else
     {
@@ -131,38 +132,14 @@ public:
       int x = x0;
       for (int y = y0; y != y1; y += dy)
       {
-        if (accumulate > threshold) {
+        if (accumulate > threshold)
+        {
           x += dx;
           threshold += 1;
         }
         pixels[y][x] = c;
         accumulate += mInvAbs;
-      }        
-    }
-  }
-
-  void lineBresenhamAdvanced(int x0, int y0, int x1, int y1, Color c)
-  {
-    int dx = std::abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-    int dy = -std::abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
-    int err = dx + dy, e2; /* error value e_xy */
-
-    for (;;)
-    {
-      pixels[y0][x0] = c;
-      if (x0 == x1 && y0 == y1)
-        break;
-      e2 = 2 * err;
-      if (e2 >= dy)
-      {
-        err += dy;
-        x0 += sx;
-      } /* e_xy+e_x > 0 */
-      if (e2 <= dx)
-      {
-        err += dx;
-        y0 += sy;
-      } /* e_xy+e_y < 0 */
+      }
     }
   }
 
