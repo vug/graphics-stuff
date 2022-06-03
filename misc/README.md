0e07fa1d8d15fc0029809ce960b1b382b6263fc1
Uğur Güney <ugurguney@gmail.com>
2022-06-03 17:35:09 -0400

    Custom texture displayed in ImGui::Image.

--- misc/README.md
+++ misc/README.md
@@ -3,3 +3,9 @@
 ```cmd
 cl /std:c++20 /W4 /external:I"../dependencies" /external:W0 /I"../dependencies/glfw/include" ../dependencies/glfw/build/src/Release/glfw3.lib /I"../dependencies/glad/include" ../dependencies/glad/glad.lib /I"../dependencies/glm" ../dependencies/glm/build/glm/Release/glm_static.lib Opengl32.lib User32.lib Gdi32.lib Shell32.lib /MD /EHsc icosahedron.cpp
 ```
+
+```cmd
+cl /std:c++20 /W4 /external:I"../dependencies" /external:W0 /I"../dependencies/glm" ../dependencies/glm/build/glm/Release/glm_static.lib -DIMGUI_USER_CONFIG="\"myimconfig.h\"" /I"../dependencies/imgui" /I"../dependencies/imgui/backends" ../dependencies/imgui/build/imgui.lib /I"../dependencies/glfw/include" ../dependencies/glfw/build/src/Release/glfw3.lib /I"../dependencies/glad/include" ../dependencies/glad/glad.lib Opengl32.lib User32.lib Gdi32.lib Shell32.lib /MD /EHsc colorpicker.cpp
+```
+
+`myimconfig.h` trick is confusing for VS Code.
---
+++ misc/colorpicker.cpp
@@ -0,0 +1,135 @@
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void glfw_error_callback(int error, const char *description);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+int width = 800;
+int height = 600;
+
+int main()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_SAMPLES, 8);
+
+  GLFWwindow *window = glfwCreateWindow(width, height, "Skeleton", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetErrorCallback(glfw_error_callback);
+
+  gladLoadGL();
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+
+  ImGui::StyleColorsDark();
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+  ImGuiStyle &style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+  {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 460");
+
+  bool showDemo = false;
+  float clearColor[3] = {0.2f, 0.3f, 0.3f};
+
+  int image_width = 256;
+  int image_height = 256;
+  unsigned char *image_data = new unsigned char[image_height * image_width * 4];
+  for (int i = 0; i < image_height; ++i)
+    for (int j = 0; j < image_width; ++j)
+    {
+      const int ix = (i * image_width + j) * 4;
+      image_data[ix + 0] = static_cast<unsigned char>(i);
+      image_data[ix + 1] = static_cast<unsigned char>(0);
+      image_data[ix + 2] = static_cast<unsigned char>(j);
+      image_data[ix + 3] = static_cast<unsigned char>(255);
+    }
+  GLuint image_texture;
+  glGenTextures(1, &image_texture);
+  glBindTexture(GL_TEXTURE_2D, image_texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    if (showDemo)
+      ImGui::ShowDemoWindow();
+
+    ImGui::Begin("Main");
+    ImGui::Checkbox("Demo", &showDemo);
+    ImGui::Image((void *)(intptr_t)image_texture, {static_cast<float>(image_width), static_cast<float>(image_height)});
+    ImGui::Separator();
+    ImGui::ColorPicker3("clear color", clearColor, ImGuiColorEditFlags_None);
+    ImGui::End();
+
+    ImGui::Render();
+
+    glClearColor(clearColor[0], clearColor[1], clearColor[2], 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+      GLFWwindow *backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwTerminate();
+  return 0;
+}
+
+void glfw_error_callback(int error, const char *description)
+{
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+void key_callback(GLFWwindow *window, int key, [[maybe_unused]] int scancode, int action, [[maybe_unused]] int mode)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int w, int h)
+{
+  width = w;
+  height = h;
+  glViewport(0, 0, width, height);
+}
