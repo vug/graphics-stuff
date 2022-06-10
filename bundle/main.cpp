192e14a865c74ca4145a907913c7bf247e7bbfea
Uğur Güney <ugurguney@gmail.com>
2022-06-09 22:34:42 -0400

    Use glad via CMake in bundle

--- bundle/main.cpp
+++ bundle/main.cpp
@@ -1,37 +1,45 @@
+/**
+ * bring OpenGL functions via Glad. Set clear color.
+ */
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-int main(void)
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+int main()
 {
-    GLFWwindow* window;
+    const int width = 800;
+    const int height = 600;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
+    GLFWwindow *window = glfwCreateWindow(width, height, "OpenGL w/GLFW + Glad", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
 
-    /* Loop until the user closes the window */
+    gladLoadGL(glfwGetProcAddress);
+
+    glViewport(0, 0, width, height);
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, [[maybe_unused]] int scancode, int action, [[maybe_unused]] int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
