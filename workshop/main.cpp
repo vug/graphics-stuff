b0746d47cc4063eb1ba9180c23960dbb3f9022d2
Uğur Güney <ugurguney@gmail.com>
2022-06-12 10:55:02 -0400

    [ws] Use camera in main

--- workshop/main.cpp
+++ workshop/main.cpp
@@ -1,12 +1,11 @@
-/**
- * bring OpenGL functions via Glad. Set clear color.
- */
 #include "App.h"
 #include "Shader.h"
 #include "Mesh.h"
+#include "Camera.h"
 
 #include <glad/gl.h>
 #include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <memory>
@@ -20,6 +19,7 @@ public:
   std::unique_ptr<ws::Shader> mainShader;
   ws::OMesh *oMesh;
   std::unique_ptr<ws::Mesh> mesh;
+  std::unique_ptr<ws::Camera> camera;
 
   Specs getSpecs() final
   {
@@ -30,10 +30,17 @@ public:
   {
     const char *vertexShaderSource = R"(
 #version 460 core
-layout (location = 0) in vec3 aPos;
+
+layout (location = 0) in vec3 vPos;
+layout (location = 1) in vec3 vNorm;
+
+uniform mat4 WorldFromObject;
+uniform mat4 ViewFromWorld;
+uniform mat4 ProjectionFromView;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+  gl_Position = ProjectionFromView * ViewFromWorld * WorldFromObject * vec4(vPos, 1.0);
 }
 )";
 
@@ -42,13 +49,15 @@ void main()
 out vec4 FragColor;
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
 )";
     mainShader = std::make_unique<ws::Shader>(vertexShaderSource, fragmentShaderSource);
 
     oMesh = ws::makeIcosphereOMesh(1);
     mesh.reset(ws::makeMeshFromOMesh(*oMesh));
+
+    camera = std::make_unique<ws::Camera>(static_cast<float>(specs.width), static_cast<float>(specs.height));
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   }
@@ -66,6 +75,10 @@ void main()
       ImGui::ShowDemoWindow();
 
     glUseProgram(mainShader->id);
+    mainShader->setMatrix4fv("ViewFromWorld", glm::value_ptr(camera->getViewFromWorld()));
+    mainShader->setMatrix4fv("ProjectionFromView", glm::value_ptr(camera->getProjectionFromView()));
+
+    mainShader->setMatrix4fv("WorldFromObject", glm::value_ptr(glm::mat4(1.f)));
     glBindVertexArray(mesh->vao);
     glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(mesh->idxs.size()), GL_UNSIGNED_INT, 0);
   }
