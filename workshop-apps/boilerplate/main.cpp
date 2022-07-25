d4d7b32180ccb3662f1cafb8c59878e7fd9e8635
Uğur Güney <ugurguney@gmail.com>
2022-07-25 16:35:05 -0400

    put all shaders into an `unordered_map`

--- workshop-apps/boilerplate/main.cpp
+++ workshop-apps/boilerplate/main.cpp
@@ -11,12 +11,13 @@
 #include <implot.h>
 
 #include <memory>
+#include <string>
+#include <unordered_map>
 
 class Boilerplate : public ws::App
 {
 public:
-  std::unique_ptr<ws::Shader> shader;
-  std::unique_ptr<ws::Shader> shaderQuad;
+  std::unordered_map<std::string, std::unique_ptr<ws::Shader>> shaders;
   std::unique_ptr<ws::Mesh> mesh;
   std::unique_ptr<ws::Mesh> meshQuad;
   std::unique_ptr<ws::Framebuffer> framebuffer;
@@ -25,12 +26,13 @@ public:
 
   void onInit() final
   {
-    shader = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/graverlet/main.vert",
-                                          GS_ASSETS_FOLDER / "shaders/graverlet/line.frag");
+    shaders["main"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/graverlet/main.vert",
+                                                   GS_ASSETS_FOLDER / "shaders/graverlet/line.frag");
+    shaders["quad"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/postprocess/main.vert",
+                                                   GS_ASSETS_FOLDER / "shaders/postprocess/main.frag");
+
     mesh.reset(new ws::Mesh(ws::Mesh::makeQuadLines()));
 
-    shaderQuad = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/postprocess/main.vert",
-                                              GS_ASSETS_FOLDER / "shaders/postprocess/main.frag");
     meshQuad.reset(new ws::Mesh(ws::Mesh::makeQuad()));
 
     framebuffer = std::make_unique<ws::Framebuffer>(width, height);
@@ -53,29 +55,35 @@ public:
       ImPlot::ShowDemoWindow();
     ImGui::Separator();
     if (ImGui::Button("Reload"))
-      shaderQuad->reload();
+      for (auto &[name, shader] : shaders)
+        shader->reload();
     ImGui::End();
 
-    framebuffer->bind();
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    {
+      framebuffer->bind();
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      ws::Shader &shader = *shaders["main"];
+      shader.bind();
+      shader.setVector2fv("RenderTargetSize", renderTargetSize);
+      auto proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.f, 1.f);
+      shader.setMatrix4fv("ProjectionFromView", glm::value_ptr(proj));
+      mesh->bind();
+      mesh->draw();
+      framebuffer->unbind();
+    }
 
-    shader->bind();
-    shader->setVector2fv("RenderTargetSize", renderTargetSize);
-    auto proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.f, 1.f);
-    shader->setMatrix4fv("ProjectionFromView", glm::value_ptr(proj));
-    mesh->bind();
-    mesh->draw();
-    framebuffer->unbind();
-
-    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    shaderQuad->bind();
-    shaderQuad->setVector2fv("RenderTargetSize", renderTargetSize);
-    meshQuad->bind();
-    glDisable(GL_DEPTH_TEST);
-    glBindTexture(GL_TEXTURE_2D, framebuffer->getColorAttachment());
-    meshQuad->draw();
+    {
+      glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      ws::Shader &shader = *shaders["quad"];
+      shader.bind();
+      shader.setVector2fv("RenderTargetSize", renderTargetSize);
+      meshQuad->bind();
+      glDisable(GL_DEPTH_TEST);
+      glBindTexture(GL_TEXTURE_2D, framebuffer->getColorAttachment());
+      meshQuad->draw();
+    }
   }
 
   void onDeinit() final
