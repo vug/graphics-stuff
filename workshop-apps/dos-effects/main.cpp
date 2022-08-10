bafbec7b5dd07f6b589a6e980599010b67d0ee10
Uğur Güney <ugurguney@gmail.com>
2022-08-10 15:04:31 -0400

    Better separation of "DOS demos"

--- workshop-apps/dos-effects/main.cpp
+++ workshop-apps/dos-effects/main.cpp
@@ -22,12 +22,8 @@ public:
   std::unordered_map<std::string, std::unique_ptr<ws::Shader>> shaders;
   std::unique_ptr<ws::Mesh> mesh;
   std::unique_ptr<ws::Mesh> meshQuad;
-  std::unique_ptr<ws::Framebuffer> framebuffer;
-  std::unique_ptr<ws::Framebuffer> framebuffer2;
-  std::unique_ptr<ws::Texture> image;
   std::mt19937 rng;
   std::uniform_real_distribution<float> dist;
-  uint8_t *imgSnow = new uint8_t[3 * height * width];
 
   Boilerplate() : App({.name = "MyApp", .width = 1280u, .height = 960u, .shouldDebugOpenGL = true}) {}
 
@@ -36,30 +32,12 @@ public:
     rng = std::mt19937{std::random_device{}()};
     dist = std::uniform_real_distribution<float>();
 
-    shaders["main"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/graverlet/main.vert",
-                                                   GS_ASSETS_FOLDER / "shaders/graverlet/line.frag");
     shaders["quad"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/postprocess/main.vert",
                                                    GS_ASSETS_FOLDER / "shaders/postprocess/main.frag");
 
     mesh.reset(new ws::Mesh(ws::Mesh::makeQuadLines()));
 
     meshQuad.reset(new ws::Mesh(ws::Mesh::makeQuad()));
-
-    framebuffer = std::make_unique<ws::Framebuffer>(width, height);
-
-    image = std::make_unique<ws::Texture>(ws::Texture::Specs{width, height, ws::Texture::Format::RGB8, ws::Texture::Filter::Nearest, ws::Texture::Wrap::Repeat});
-    image = std::make_unique<ws::Texture>(ws::Texture::Specs{160, 120, ws::Texture::Format::RGB8, ws::Texture::Filter::Nearest, ws::Texture::Wrap::Repeat});
-
-    for (uint32_t i = 0; i < height; ++i)
-    {
-      for (uint32_t j = 0; j < width; ++j)
-      {
-        size_t ix = 3 * (i * width + j);
-        imgSnow[ix + 0] = 0;
-        imgSnow[ix + 1] = 0;
-        imgSnow[ix + 2] = 0;
-      }
-    }
   }
 
   void onRender([[maybe_unused]] float time, [[maybe_unused]] float deltaTime) final
@@ -68,7 +46,14 @@ public:
     const float heightF = static_cast<float>(height);
     const float renderTargetSize[2] = {widthF, heightF};
 
-    ImGui::Begin("Boilerplate");
+    ImGui::Begin("DOS Effects");
+
+    static int demoNo{};
+    ImGui::Combo("Demo", &demoNo, "Sample Scene into FB\0Snow\0UV Gradient\0");
+    if (ImGui::Button("Reload Shaders"))
+      for (auto &[name, shader] : shaders)
+        shader->reload();
+    ImGui::Separator();
     static bool showImGuiDemo = false;
     static bool showImPlotDemo = false;
     ImGui::Checkbox("Show ImGui Demo", &showImGuiDemo);
@@ -77,27 +62,92 @@ public:
       ImGui::ShowDemoWindow();
     if (showImPlotDemo)
       ImPlot::ShowDemoWindow();
-    ImGui::Separator();
-    if (ImGui::Button("Reload"))
-      for (auto &[name, shader] : shaders)
-        shader->reload();
     ImGui::End();
 
+    uint32_t textureId{};
+    switch (demoNo)
     {
-      framebuffer->bind();
-      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      ws::Shader &shader = *shaders["main"];
-      shader.bind();
-      shader.setVector2fv("RenderTargetSize", renderTargetSize);
-      auto proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.f, 1.f);
-      shader.setMatrix4fv("ProjectionFromView", glm::value_ptr(proj));
-      mesh->bind();
-      mesh->draw();
-      framebuffer->unbind();
+    case 0:
+      textureId = drawASceneIntoAFrameBufferEffect();
+      break;
+    case 1:
+      textureId = snowEffect();
+      break;
+    case 2:
+      textureId = uvGradientEffect();
+      break;
     }
 
-    static uint32_t frameNo = 0;
+    {
+      glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      ws::Shader &shader = *shaders["quad"];
+      shader.bind();
+      shader.setVector2fv("RenderTargetSize", renderTargetSize);
+      meshQuad->bind();
+      glDisable(GL_DEPTH_TEST);
+      glBindTexture(GL_TEXTURE_2D, textureId);
+      meshQuad->draw();
+    }
+  }
+
+  void onDeinit() final
+  {
+  }
+
+  uint32_t drawASceneIntoAFrameBufferEffect()
+  {
+    // TODO: make renderTargetSize widely available
+    const float widthF = static_cast<float>(width);
+    const float heightF = static_cast<float>(height);
+    const float renderTargetSize[2] = {widthF, heightF};
+
+    static std::unique_ptr<ws::Framebuffer> framebuffer = std::make_unique<ws::Framebuffer>(width, height);
+    static std::unique_ptr<ws::Shader> shaderMain = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/graverlet/main.vert",
+                                                                                 GS_ASSETS_FOLDER / "shaders/graverlet/line.frag");
+    if (!shaders.contains("main"))
+      shaders["main"] = std::move(shaderMain);
+
+    framebuffer->bind();
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    ws::Shader &shader = *shaders["main"];
+    shader.bind();
+    shader.setVector2fv("RenderTargetSize", renderTargetSize);
+    auto proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.f, 1.f);
+    shader.setMatrix4fv("ProjectionFromView", glm::value_ptr(proj));
+    mesh->bind();
+    mesh->draw();
+    framebuffer->unbind();
+    return framebuffer->getColorAttachment().getId();
+  }
+
+  uint32_t uvGradientEffect()
+  {
+    static std::unique_ptr<ws::Texture> image = std::make_unique<ws::Texture>(ws::Texture::Specs{width, height, ws::Texture::Format::RGB8, ws::Texture::Filter::Nearest, ws::Texture::Wrap::Repeat});
+    const auto &h = image->specs.height;
+    const auto &w = image->specs.width;
+    static uint8_t *imgData = new uint8_t[3 * h * w];
+
+    for (uint32_t i = 0; i < h; ++i)
+    {
+      for (uint32_t j = 0; j < w; ++j)
+      {
+        size_t ix = 3 * (i * w + j);
+        imgData[ix + 0] = static_cast<uint8_t>(static_cast<float>(j) / w * 255);
+        imgData[ix + 1] = static_cast<uint8_t>(static_cast<float>(i) / h * 255);
+        imgData[ix + 2] = 0;
+      }
+    }
+    image->loadPixels(imgData);
+    return image->getId();
+  }
+
+  uint32_t snowEffect()
+  {
+    static std::unique_ptr<ws::Texture> image = std::make_unique<ws::Texture>(ws::Texture::Specs{160, 120, ws::Texture::Format::RGB8, ws::Texture::Filter::Nearest, ws::Texture::Wrap::Repeat});
+    static uint8_t *imgSnow = new uint8_t[3 * image->specs.height * image->specs.width]{}; // {} initializes with zeroes
+
     uint32_t snowInitX = static_cast<uint32_t>((image->specs.height - 1) * image->specs.width + dist(rng) * image->specs.width);
     imgSnow[snowInitX * 3] = 255;
 
@@ -161,38 +211,7 @@ public:
       }
     }
     image->loadPixels(imgSnow);
-
-    // static uint8_t *imgData = new uint8_t[3 * height * width];
-    // for (uint32_t i = 0; i < height; ++i)
-    // {
-    //   for (uint32_t j = 0; j < width; ++j)
-    //   {
-    //     size_t ix = 3 * (i * width + j);
-    //     imgData[ix + 0] = static_cast<uint8_t>(static_cast<float>(j) / width * 255);
-    //     imgData[ix + 1] = static_cast<uint8_t>(static_cast<float>(i) / height * 255);
-    //     imgData[ix + 2] = 0;
-    //   }
-    // }
-    // image->loadPixels(imgData);
-
-    {
-      glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
-      ws::Shader &shader = *shaders["quad"];
-      shader.bind();
-      shader.setVector2fv("RenderTargetSize", renderTargetSize);
-      meshQuad->bind();
-      glDisable(GL_DEPTH_TEST);
-      // glBindTexture(GL_TEXTURE_2D, framebuffer->getColorAttachment().getId());
-      glBindTexture(GL_TEXTURE_2D, image->getId());
-      meshQuad->draw();
-    }
-
-    ++frameNo;
-  }
-
-  void onDeinit() final
-  {
+    return image->getId();
   }
 };
 
