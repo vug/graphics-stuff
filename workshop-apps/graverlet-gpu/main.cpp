4777d6b2538277afcaf448d76cf904f34b508c58
Uğur Güney <ugurguney@gmail.com>
2022-08-16 23:25:27 -0400

    Start GraverletGPU project

---
+++ workshop-apps/graverlet-gpu/main.cpp
@@ -0,0 +1,99 @@
+#include <App.h>
+#include <Framebuffer.h>
+#include <GSAssets.h>
+#include <Mesh.h>
+#include <Shader.h>
+
+#include <glad/gl.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <imgui.h>
+#include <implot.h>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+class GraverletGPU : public ws::App
+{
+public:
+  std::unordered_map<std::string, std::unique_ptr<ws::Shader>> shaders;
+  std::unique_ptr<ws::Mesh> mesh;
+  std::unique_ptr<ws::Mesh> meshQuad;
+  std::unique_ptr<ws::Framebuffer> framebuffer;
+  std::unique_ptr<ws::Framebuffer> framebuffer2;
+
+  GraverletGPU() : App({.name = "MyApp", .width = 800u, .height = 600u, .shouldDebugOpenGL = true}) {}
+
+  void onInit() final
+  {
+    shaders["main"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/graverlet/main.vert",
+                                                   GS_ASSETS_FOLDER / "shaders/graverlet/line.frag");
+    shaders["quad"] = std::make_unique<ws::Shader>(GS_ASSETS_FOLDER / "shaders/postprocess/main.vert",
+                                                   GS_ASSETS_FOLDER / "shaders/postprocess/main.frag");
+
+    mesh.reset(new ws::Mesh(ws::Mesh::makeQuadLines()));
+
+    meshQuad.reset(new ws::Mesh(ws::Mesh::makeQuad()));
+
+    framebuffer = std::make_unique<ws::Framebuffer>(width, height);
+  }
+
+  void onRender([[maybe_unused]] float time, [[maybe_unused]] float deltaTime) final
+  {
+    const float widthF = static_cast<float>(width);
+    const float heightF = static_cast<float>(height);
+    const float renderTargetSize[2] = {widthF, heightF};
+
+    ImGui::Begin("Boilerplate");
+    static bool showImGuiDemo = false;
+    static bool showImPlotDemo = false;
+    ImGui::Checkbox("Show ImGui Demo", &showImGuiDemo);
+    ImGui::Checkbox("Show ImPlot Demo", &showImPlotDemo);
+    if (showImGuiDemo)
+      ImGui::ShowDemoWindow();
+    if (showImPlotDemo)
+      ImPlot::ShowDemoWindow();
+    ImGui::Separator();
+    if (ImGui::Button("Reload"))
+      for (auto &[name, shader] : shaders)
+        shader->reload();
+    ImGui::End();
+
+    {
+      framebuffer->bind();
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      ws::Shader &shader = *shaders["main"];
+      shader.bind();
+      shader.setVector2fv("RenderTargetSize", renderTargetSize);
+      auto proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.f, 1.f);
+      shader.setMatrix4fv("ProjectionFromView", glm::value_ptr(proj));
+      mesh->bind();
+      mesh->draw();
+      framebuffer->unbind();
+    }
+
+    {
+      glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      ws::Shader &shader = *shaders["quad"];
+      shader.bind();
+      shader.setVector2fv("RenderTargetSize", renderTargetSize);
+      meshQuad->bind();
+      glDisable(GL_DEPTH_TEST);
+      glBindTexture(GL_TEXTURE_2D, framebuffer->getColorAttachment().getId());
+      meshQuad->draw();
+    }
+  }
+
+  void onDeinit() final
+  {
+  }
+};
+
+int main()
+{
+  GraverletGPU app;
+  app.run();
+}
